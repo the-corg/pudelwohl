5eeb50f48c2553ee46fc6715fabf59e3fc3929e9
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-02-27 15:33:37 +0100

    Implement assignment of unique IDs to new guests via static NextId

--- Data/GuestDataProvider.cs
+++ Data/GuestDataProvider.cs
@@ -36,6 +36,7 @@ namespace Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Data
                            };
                 newList.AddRange(data);
             }
+            Guest.CalculateNextId(newList);
             return newList;
         }
     }
--- Model/Guest.cs
+++ Model/Guest.cs
@@ -2,7 +2,7 @@
 {
     public class Guest
     {
-        public int Id { get; set; }
+        public int Id { get; set; } = NextId;
         public string Name { get; set; }
         public string? Breed { get; set; }
         public Gender Gender { get; set; }
@@ -13,6 +13,26 @@
         public string? SpecialRequests { get; set; }
         public bool IsArchived { get; set; }
 
+
+        // Static field and property for automatic ID assignment
+        private static int _nextId = 0;
+        public static int NextId
+        { 
+            get => _nextId++;
+            private set => _nextId = value;
+        }
+
+        private static bool nextIdAlreadyCalculated = false;
+        // Calculate NextId based on the maximum of all IDs.
+        // Works only once, should be called after initial data loading
+        public static void CalculateNextId(List<Guest> guests)
+        {
+            if (nextIdAlreadyCalculated) 
+                return;
+
+            nextIdAlreadyCalculated = true;
+            NextId = guests.Max(guest => guest.Id) + 1;
+        }
     }
 
     public enum Gender
