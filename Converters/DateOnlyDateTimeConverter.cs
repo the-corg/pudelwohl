6b924feba8a73a79941509ebeff4f49a2c51507a
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-02-28 21:09:17 +0100

    Add DateOnly to DateTime converter

---
+++ Converters/DateOnlyDateTimeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Converters
+{
+    class DateOnlyDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateOnly?)value)?.ToDateTime(TimeOnly.MinValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is null ? null : DateOnly.FromDateTime(((DateTime)value));
+        }
+    }
+}
