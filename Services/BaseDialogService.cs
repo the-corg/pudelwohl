b015090fcbaf130c05e762ad44645dc05a61adc5
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-03-03 14:43:58 +0100

    Create base class for dialog services and move common logic there

---
+++ Services/BaseDialogService.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Services
+{
+    public class BaseDialogService
+    {
+        private Window? _mainWindow;
+        private Window? MainWindow => _mainWindow ??= Application.Current.MainWindow; // Lazy initialization
+
+        protected bool ShowDialog(Window dialog)
+        {
+            // Dim main window before showing the modal window, then restore it back
+            if (MainWindow is not null) MainWindow.Opacity = 0.4;
+            bool? result = dialog.ShowDialog();
+            if (MainWindow is not null) MainWindow.Opacity = 1.0;
+            return result == true;
+        }
+    }
+}
--- Services/BookingDialogService.cs
+++ Services/BookingDialogService.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows;
-using Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Model;
+﻿using Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Model;
 using Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Services.Data;
 using Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.View;
 using Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.ViewModel;
@@ -11,14 +10,10 @@ namespace Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Service
         bool ShowBookingDialog(string headerText, bool isGuestSelectable, bool isRoomSelectable, int guestId, int roomId, Booking? booking = null);
     }
 
-    public class BookingDialogService : IBookingDialogService
+    public class BookingDialogService : BaseDialogService, IBookingDialogService
     {
         private readonly IGuestDataService _guestDataService;
         private readonly IRoomDataService _roomDataService;
-
-        private Window? _mainWindow;
-        private Window? MainWindow => _mainWindow ??= Application.Current.MainWindow; // Lazy initialization
-        
 
         public BookingDialogService(IGuestDataService guestDataService, IRoomDataService roomDataService)
         {
@@ -28,17 +23,12 @@ namespace Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Service
 
         public bool ShowBookingDialog(string headerText, bool isGuestSelectable, bool isRoomSelectable, int guestId, int roomId, Booking? booking = null)
         {
-            var dialogWindow = new BookingDetails();
+            var dialog = new BookingDetails();
             var viewModel = new BookingDetailsViewModel(_guestDataService, _roomDataService, headerText, 
                 isGuestSelectable, isRoomSelectable, guestId, roomId, booking);
-            dialogWindow.DataContext = viewModel;
-            viewModel.CloseOnConfirmAction = () => dialogWindow.DialogResult = true;
-
-            // Dim main window before showing the modal window, then restore it back
-            if (MainWindow is not null) MainWindow.Opacity = 0.4;
-            bool? result = dialogWindow.ShowDialog();
-            if (MainWindow is not null) MainWindow.Opacity = 1.0;
-            return result == true;
+            dialog.DataContext = viewModel;
+            viewModel.CloseOnConfirmAction = () => dialog.DialogResult = true;
+            return ShowDialog(dialog);
         }
     }
 }
