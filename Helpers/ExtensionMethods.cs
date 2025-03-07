f70ad60b73c4a0436720b9c60c70b96fab24e7c8
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-03-07 15:47:05 +0100

    Implement tooltip when selected item doesn't fit into combobox

---
+++ Helpers/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Media;
+using System.Windows;
+
+namespace Pudelwohl_Hotel_and_Resort_Management_Suite_Ultimate_Wuff_Wuff.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static T? GetVisualChildOfType<T>(this DependencyObject? dependencyObject)
+        where T : DependencyObject
+        {
+            if (dependencyObject is null)
+                return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(dependencyObject); i++)
+            {
+                var child = VisualTreeHelper.GetChild(dependencyObject, i);
+                var result = (child as T) ?? GetVisualChildOfType<T>(child);
+
+                if (result is not null)
+                    return result;
+            }
+            return null;
+        }
+    }
+}
